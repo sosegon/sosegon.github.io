0f35069f82d8b14b7ddedbcd3f5cef9df0bbac11
Sebastian <anse23@hotmail.com>
2019-04-02 20:12:09 -0500

    conf: Set gulp to transpile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', () => {
-	console.log('running gulp');
+const styles = 'src/style/**/*.scss';
+
+gulp.task('styles', () => {
+	return gulp.src(styles)
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./built/style'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch(styles, ['styles']);
 });
