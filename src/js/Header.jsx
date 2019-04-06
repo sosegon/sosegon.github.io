a5bac4f1ee24d186710f122fc01517c5b9ab9f21
Sebastian <anse23@hotmail.com>
2019-04-05 21:48:25 -0500

    ui: Added logo to nav bar

--- src/js/Header.jsx
+++ src/js/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Header = () => (
-	<div className="header">
+	<div className="header sticky-top navbar-light bg-light">
     <div className="header-cont">
+      <div className="logo">
+        SV
+      </div>
       <nav className="navbar navbar-expand-sm">
         <span className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <img src="assets/images/bars.png" className="d-block"/>
-          <img src="assets/images/close.png" className="d-none"/>
+          <img src="../images/bars.png" className="d-block"/>
+          <img src="../images/close.png" className="d-none"/>
         </span>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
