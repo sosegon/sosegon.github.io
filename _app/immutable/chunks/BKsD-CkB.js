8695c825c27e4178303a255df072d5c9b5c88c45
Sebastian Velasquez <anse23@hotmail.com>
2025-02-11 15:24:00 -0500

    Adds version 2 of se-vel

---
+++ _app/immutable/chunks/BKsD-CkB.js
@@ -0,0 +1,1 @@
+import{t as i,a as r}from"./DBNrgphE.js";import"./DBovM6ZJ.js";var v=i('<div class="svelte-vhhwha"></div>');function p(a){var t=v();r(a,t)}export{p as D};
---
+++ _app/immutable/entry/start.BFHsrrt7.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/pDkNMT5z.js";export{t as start};
