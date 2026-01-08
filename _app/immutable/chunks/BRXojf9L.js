7b79f2827507fddbdff0445e23747d41b03a8e98
Sebastian Velasquez <anse23@hotmail.com>
2026-01-08 12:50:25 -0500

    conf: Removes unneeded code and adds README

--- _app/immutable/chunks/BRXojf9L.js
+++
@@ -1,1 +0,0 @@
-import{a5 as a}from"./D4-vUi9C.js";a();
--- storybook/assets/slot-B1isFuVn.js
+++
@@ -1,1 +0,0 @@
-function r(t,f,a,n,s){var l;var e=(l=f.$$slots)==null?void 0:l[a],i=!1;e===!0&&(e=f.children,i=!0),e===void 0||e(t,i?()=>n:n)}export{r as s};
